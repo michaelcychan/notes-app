3e5f104a53424e646869d6de66f241fb03477c1a
Michael Chan <michaelcychan@gmail.com>
2022-07-04 17:06:19 +0100

    list in console log upon opening

--- bundle.js
+++ bundle.js
@@ -29,5 +29,6 @@
   var NotesModel = require_notesModel();
   var model = new NotesModel();
   console.log("The notes app is running");
+  console.log("The list has been initialised. The current notes:");
   console.log(model.getNotes());
 })();
--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const model = new NotesModel();
 
 console.log('The notes app is running');
 
+console.log('The list has been initialised. The current notes:');
 console.log(model.getNotes());
