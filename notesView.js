a8d1f0c789cf7ce6e8765956fbf1a76e775ea0de
Michael Chan <michaelcychan@gmail.com>
2022-07-05 15:30:55 +0100

    notesview displaynotes()

--- notesView.js
+++ notesView.js
@@ -1,5 +1,17 @@
 class NotesView {
-
+  constructor(model) {
+    this.mainContainerEl = document.querySelector('#main-container');
+    this.model = model;
+  }
+  displayNotes() {
+    this.model.getNotes().forEach((eachNote) => {
+      let div = document.createElement('div');
+      div.setAttribute("class", "note"); // div.className = "note"
+      div.append(eachNote);  // div.innerText = eachNote
+      div.setAttribute("id", "Test");
+      this.mainContainerEl.append(div);
+    });
+  }
 }
 
 module.exports = NotesView;
--- tests/notesView.test.js
+++ tests/notesView.test.js
@@ -7,7 +7,7 @@ const NotesView = require('../notesView');
 const fs = require('fs');
 
 describe('NotesView', () => {
-  it('displays the notes', () => {
+  it('displays one note', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
     const model = new NotesModel();
     const view = new NotesView(model);
@@ -15,5 +15,17 @@ describe('NotesView', () => {
     
     view.displayNotes();
     expect(document.body.querySelectorAll('div.note').length).toStrictEqual(1);
-  })
+  });
+
+  it('displays multiple notes', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    model.addNote('This is the first Note');
+    model.addNote('This is the second Note');
+    model.addNote('This is the third Note');
+    view.displayNotes();
+    expect(document.body.querySelectorAll('div.note').length).toStrictEqual(3);
+    expect(document.body.querySelectorAll('#Test').length).toStrictEqual(3)
+  });
 })
